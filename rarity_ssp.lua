e3f63a734a5840f153f19bb0409cb7aa9eef0c0b
Helios666x <jalankelincis@gmail.com>
2026-06-21 20:45:20 +0700

    1

---
+++ rarity_ssp.lua
@@ -0,0 +1,184 @@
+-- ============================================================
+--   SSP RARITY DATABASE (EXTERNAL CLOUD FILE)
+--   Source: Growtopia Item Database Dump
+-- ============================================================
+
+SSP_RARITY = {
+    -- [ Rarity 2 ]
+    [3567] = 2, -- Cave Column Seed
+    [13]   = 2, -- Door Seed
+    [57]   = 2, -- Glass Pane Seed
+    [17]   = 2, -- Grass Seed
+    [381]  = 2, -- Lava Rock Seed
+    [1139] = 2, -- Martian Tree Seed
+    [21]   = 2, -- Sign Seed
+    [101]  = 2, -- Wood Block Seed
+
+    -- [ Rarity 3 ]
+    [379]  = 3, -- Amber Glass Seed
+    [117]  = 3, -- Bricks Seed
+    [27]   = 3, -- Crappy Sign Seed
+    [23]   = 3, -- Daisy Seed
+    [581]  = 3, -- Happy Joy Plaque Seed
+    [195]  = 3, -- Mushroom Seed
+    [105]  = 3, -- Rock Background Seed
+    [191]  = 3, -- Rose Seed
+    [377]  = 3, -- Super Crate Box Seed
+    [1307] = 3, -- Terracotta Pot Seed
+    [697]  = 3, -- Torch Seed
+    [881]  = 3, -- Wheat Seed
+
+    -- [ Rarity 4 ]
+    [167]  = 4, -- Black Block Seed
+    [185]  = 4, -- Brown Block Seed
+    [1323] = 4, -- Cliffside Seed
+    [165]  = 4, -- Grey Block Seed
+    [3783] = 4, -- Mini Blocks Seed
+    [369]  = 4, -- Mud Glob Seed
+    [25]   = 4, -- Pointy Sign Seed
+    [171]  = 4, -- Red Block Seed
+    [53]   = 4, -- Wooden Background Seed
+    [103]  = 4, -- Wooden Platform Seed
+
+    -- [ Rarity 5 ]
+    [179]  = 5, -- Aqua Block Seed
+    [29]   = 5, -- Danger Sign Seed
+    [249]  = 5, -- Evil Bricks Seed
+    [177]  = 5, -- Green Block Seed
+    [1325] = 5, -- Rock Platform Seed
+    [337]  = 5, -- Stone Wall Seed
+
+    -- [ Rarity 6 ]
+    [189]  = 6, -- Poppy Seed
+    [169]  = 6, -- White Block Seed
+    [55]   = 6, -- Window Seed
+    [175]  = 6, -- Yellow Block Seed
+
+    -- [ Rarity 7 ]
+    [885]  = 7, -- Barn Block Seed
+    [119]  = 7, -- Brick Background Seed
+    [3573] = 7, -- Cave Platform Seed
+    [199]  = 7, -- Flowery Wallpaper Seed
+    [549]  = 7, -- Glass Block Seed
+    [681]  = 7, -- Grimstone Seed
+    [655]  = 7, -- Red Bricks Seed
+    [107]  = 7, -- Toilet Seed
+
+    -- [ Rarity 8 ]
+    [163]  = 8, -- Death Spikes Seed
+    [31]   = 8, -- Dungeon Door Seed
+    [671]  = 8, -- Pencil Seed
+    [889]  = 8, -- Red Wood Wall Seed
+
+    -- [ Rarity 9 ]
+    [887]  = 9, -- Barn Door Seed
+    [193]  = 9, -- Bush Seed
+    [127]  = 9, -- Devil Horns Seed
+    [1433] = 9, -- Exclamation Sign Seed
+    [173]  = 9, -- Orange Block Seed
+    [413]  = 9, -- Sheet Music: Blank Seed
+    [3571] = 9, -- Stalactite Seed
+    [3569] = 9, -- Stalagmite Seed
+    [2809] = 9, -- Texas Limestone Seed
+
+    -- [ Rarity 10 ]
+    [225]  = 10, -- House Entrance Seed
+    [111]  = 10, -- Painting: Dink Duck Seed
+    [109]  = 10, -- Painting: Yerfdog Seed
+    [201]  = 10, -- Stripey Wallpaper Seed
+    [59]   = 10, -- Wooden Window Seed
+
+    -- [ Rarity 11 ]
+    [607]  = 11, -- Barrel Seed
+    [285]  = 11, -- Checker Wallpaper Seed
+    [2029] = 11, -- Dark Brown Block Seed
+    [2013] = 11, -- Dark Grey Block Seed
+    [2015] = 11, -- Dark Red Block Seed
+    [1611] = 11, -- Red Glass Block Seed
+    [995]  = 11, -- Sidewalk Seed
+    [997]  = 11, -- Tenement Building Seed
+    [963]  = 11, -- Tomato Seed
+    [1481] = 11, -- Window Curtains Seed
+    [219]  = 11, -- Wooden Chair Seed
+
+    -- [ Rarity 12 ]
+    [197]  = 12, -- Blueberry Seed
+    [2023] = 12, -- Dark Aqua Block Seed
+    [2021] = 12, -- Dark Green Block Seed
+    [1305] = 12, -- Staircase Seed
+    [823]  = 12, -- Water Bucket Seed
+    [1039] = 12, -- Western Building Seed
+
+    -- [ Rarity 13 ]
+    [231]  = 13, -- Bathtub Seed
+    [487]  = 13, -- Big Old Down Arrow Seed
+    [483]  = 13, -- Big Old Sideways Arrow Seed
+    [2019] = 13, -- Dark Yellow Block Seed
+    [427]  = 13, -- Sheet Music: Drums Seed
+    [1757] = 13, -- Tiger Block Seed
+
+    -- [ Rarity 14 ]
+    [281]  = 14, -- Bed Seed
+    [781]  = 14, -- Biohazard Sign Seed
+    [329]  = 14, -- Bubble Wrap Seed
+    [459]  = 14, -- Dresser Seed
+    [1041] = 14, -- Saloon Doors Seed
+    [421]  = 14, -- Sheet Music: Piano Note Seed
+    [2787] = 14, -- Viney Block Seed
+    [2789] = 14, -- Viney Wallpaper Seed
+    [1043] = 14, -- Western Banner Seed
+
+    -- [ Rarity 15 ]
+    [2797] = 15, -- Air Duct Seed
+    [181]  = 15, -- Blue Block Seed
+    [613]  = 15, -- Lattice Background Seed
+    [65]   = 15, -- Olde Timey Radio Seed
+    [4983] = 15, -- Orange Seed
+    [283]  = 15, -- Polka Dot Block Seed
+    [553]  = 15, -- Secret Passage Seed
+    [425]  = 15, -- Sheet Music: Flat Piano Seed
+    [4635] = 15, -- Sheet Music: Sax Note Seed
+    [987]  = 15, -- Street Sign Seed
+
+    -- [ Rarity 16 ]
+    [2017] = 16, -- Dark Orange Block Seed
+    [989]  = 16, -- Gargoyle Seed
+    [61]   = 16, -- Portcullis Seed
+    [4639] = 16, -- Sheet Music: Flat Sax Seed
+
+    -- [ Rarity 17 ]
+    [973]  = 17, -- Ancient Stone Gate Seed
+    [3523] = 17, -- Arrow Placard Seed
+    [331]  = 17, -- Cactus Seed
+    [265]  = 17, -- Picket Fence Seed
+    [859]  = 17, -- Screen Door Seed
+
+    -- [ Rarity 18 ]
+    [433]  = 18, -- Blue Star Wallpaper Seed
+    [253]  = 18, -- Cuzco Wall Mount Seed
+    [339]  = 18, -- Das Red Balloon Seed
+    [1105] = 18, -- Foliage Seed
+    [439]  = 18, -- Plumbing Seed
+    [4567] = 18, -- Salt Block Seed
+    [333]  = 18, -- Sandstone Seed
+
+    -- [ Rarity 19 ]
+    [677]  = 19, -- Apple Seed
+    [1427] = 19, -- For Sale Sign Seed
+    [5603] = 19, -- Ice Crust Block Seed
+    [183]  = 19, -- Purple Block Seed
+    [5731] = 19, -- Sheet Music: Flat Flute Seed
+    [6035] = 19, -- Sheet Music: Flat Spanish Guitar Seed
+
+    -- [ Rarity 20 ]
+    [1113] = 20, -- Outie Block Seed
+    [1047] = 20, -- Rustic Fence Seed
+    [653]  = 20, -- School Desk Seed
+
+    -- [ Rarity 21 ]
+    [2811] = 21, -- Air Vent Seed
+    [4703] = 21, -- Ancient Block Seed
+    [1429] = 21, -- Gem Sign Seed
+    [847]  = 21, -- Seaweed Seed
+    [665]  = 21  -- Swiss Cheese Block Seed
+}
